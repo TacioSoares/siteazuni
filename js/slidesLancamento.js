e0bacbcbb01226074773b236204808b8677111c6
Tácio Soares <taciofsoares@gmail.com>
2023-06-30 16:12:38 -0300

    ok

--- js/slidesLancamento.js
+++ js/slidesLancamento.js
@@ -1,10 +1,29 @@
 const bannerSlideLancamento = document.querySelector('.item-do-lancamento')
 const slideLancamento = document.querySelectorAll('.item-lancamento-container')
 
-slideLancamento.forEach(anuncio => {
+
+function moveBanner(event) {
+    console.log(event.clientX)
+}
+
+
+slideLancamento.forEach((anuncio, index) => {
     anuncio.addEventListener('mouseenter', function(){
+        posicao = bannerSlideLancamento.getClientRects()
+        bannerSlideLancamento.style.transform = `translateX(${posicao[0].x}px)`
         bannerSlideLancamento.style.animation = 'none'
+        anuncio.addEventListener('mousedown', function(event) {
+            anuncio.addEventListener('dragstart', function(event) {
+                event.preventDefault()
+            })
+            anuncio.addEventListener('mousemove', moveBanner)
+            anuncio.addEventListener('mouseup', function(){
+                console.log('soltou')
+                anuncio.removeEventListener('mousemove', moveBanner)
+            })
+        })
     })
+    
     anuncio.addEventListener('mouseout', function(){
         bannerSlideLancamento.style.animation = 'slideON 40s 1s infinite alternate'
     })
